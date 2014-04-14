ca43ccb2c6c9e132662b594b8df568062f3d73e0
urmaul <bogdan@itechcraft.com>
2014-04-14 19:51:02 +0300

    cookie test

--- test/CookieTest.php
+++ test/CookieTest.php
@@ -14,4 +14,22 @@ class CookieTest extends PHPUnit_Framework_TestCase
 		$http = HttpClient::from(array('useRandomCookieFile' => true));
 		$this->assertNotEmpty($http->getCookieFile());
 	}
+	
+	public function testCookies()
+	{
+		$http = HttpClient::from(array('useRandomCookieFile' => true));
+		
+		$actual = $http->get('http://httpbin.org/cookies/set?k1=v1&k2=v2');
+		
+		$expected = <<<JSON
+{
+  "cookies": {
+    "k1": "v1",
+    "k2": "v2"
+  }
+}
+JSON;
+		
+		$this->assertEquals($expected, $actual);
+	}
 }
